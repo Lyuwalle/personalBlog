6d80d8e9e3986e0fa224eed059dbd4a7e9f04c25
Lyuwalle <58928245+Lyuwalle@users.noreply.github.com>
2020-05-11 15:01:01 +0800

    Add files via upload

---
+++ src/main/java/com/personalBlog/mapper/DiaryMapper.java
@@ -0,0 +1,16 @@
+package com.personalblog.mapper;
+
+import com.personalblog.model.Diary;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+public interface DiaryMapper {
+
+    void writediary(Diary diary);
+
+    List<Diary> selectAllDiary();
+
+    void deleteDiaryById(int diaryid);
+
+}
